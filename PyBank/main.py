0ec111ccc95a145ad14107b066cbe0caffb91b44
shewolf <britalvr@gmail.com>
2019-05-02 16:14:58 -0500

    Add existing file

---
+++ PyBank/main.py
@@ -0,0 +1,63 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+import math
+
+csvpath = os.path.join('budget_data.csv') 
+
+# Module used for reading csv files
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+
+    total_months= 0
+    totalamount= 0
+    lastamount=0
+    change=0
+    totalchange=0
+    averagechange=0
+    greatest_decrease = 0
+    greatest_increase = 0
+    greatest_increase_month=[]
+    greatest_decrease_month=[]
+
+
+    # Read each row of data after the header
+    for row in csvreader:
+
+        total_months=total_months+1
+        totalamount=totalamount+int(row[1])
+
+        if total_months != 1:
+            change=int(row[1])-lastamount
+
+        totalchange = totalchange + change
+
+        lastamount= int(row[1])
+
+        if change > greatest_increase:
+           greatest_increase= change
+           greatest_increase_month= row[0]
+
+        if totalchange < greatest_decrease:
+           greatest_decrease= change
+           greatest_decrease_month= row[0]     
+
+averagechange = totalchange/((total_months)-1) 
+print("Financial Analysis")  
+print("-----------------------")
+print("Total Months: "+str(total_months))
+print("Total: "+"$"+str(totalamount))
+print("Average Change: "+str(round(averagechange)))
+print("Greatest Increase in Profits: "+str(greatest_increase_month)+" ($"+str(greatest_increase)+")")
+print("Greatest Decrease in Profits: "+str(greatest_decrease_month)+" ($"+str(greatest_decrease)+")")
---
+++ PyPoll/main.py
@@ -0,0 +1,47 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+import csv
+
+voter_id = []
+county = []
+candidate_votes = []
+
+csvpath = os.path.join('election_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+
+
+   # CSV reader specifies delimiter and variable that holds contents
+   csvreader = csv.reader(csvfile, delimiter=',')
+
+
+   # Read the header row first (skip this step if there is now header)
+   csv_header = next(csvreader)
+
+
+   # Read each row of data after the header
+   for row in csvreader:
+
+        voter_id.append(row[0])
+        county.append(row[1])
+        candidate_votes.append(row[2])
+
+total_votes = int(len(candidate_votes))
+khan_votes = (candidate_votes.count("Khan"))
+correy_votes = (candidate_votes.count("Correy"))
+li_votes = (candidate_votes.count("Li"))
+otooley_votes = (candidate_votes.count("O'Tooley"))
+total_votes
+
+print("Election Results")
+print("----------------------------")
+print("Total votes: " + str(len(voter_id)))
+print("----------------------------")
+print("Khan: " + str(int((khan_votes/total_votes)*100)) + "% (" + str(khan_votes) + ")")
+print("Correy: " + str(int((correy_votes/total_votes)*100)) + "% (" + str(correy_votes) + ")")
+print("Li: " + str(int((li_votes/total_votes)*100)) + "% (" + str(li_votes) + ")")
+print("O'Tooley: " + str(int((otooley_votes/total_votes)*100)) + "% (" + str(otooley_votes) + ")")
+print("----------------------------")
+print("Winner:" + "Khan" )
+print("-----------------------------")
